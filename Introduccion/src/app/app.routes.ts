f1e6617d06ce5b8e4d0f7c1f88985995caadc7d4
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-10-29 20:00:19 +0100

    up

--- Introduccion/src/app/app.routes.ts
+++ Introduccion/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Carrera } from './carrera/carrera';
 import { PasoParametros } from './paso-parametros/paso-parametros';
 import { Lista } from './ListaFrutas/lista/lista';
 import { FormularioRegistro } from './formulario-registro/formulario-registro';
+import { KanbanBoard } from './kanban-board/kanban-board';
 
 export const routes: Routes = [
     { path: 'contador', component: Contador},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'matatopos', component: Matatopos},
     { path: 'carrera', component: Carrera}, 
     { path: 'paso-parametros/:num', component: PasoParametros},
-    { path: 'formulario-registro', component: FormularioRegistro},         
+    { path: 'formulario-registro', component: FormularioRegistro},   
+    { path: 'kanban-board', component: KanbanBoard},         
 ];
--- Introduccion/src/app/kanban-board/kanban-board.ts
+++ Introduccion/src/app/kanban-board/kanban-board.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './kanban-board.html',
   styleUrl: './kanban-board.css'
 })
 export class KanbanBoard {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
+  mostarSweetAlert(){
+    Swal.fire({
+  title: "Sweet!",
+  text: "Modal with a custom image.",
+  imageUrl: "https://unsplash.it/400/200",
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: "Custom image"
+});
+  }
 
 }
+
